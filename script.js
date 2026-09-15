f74f385c629be58dad4c82d29b66b4d62b5cd3bb
Konortv <contacto@konortv.com>
2026-09-15 20:25:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-document.getElementById('year').textContent=new Date().getFullYear();
-document.querySelectorAll('a[href="#"]').forEach(a=>a.addEventListener('click',e=>e.preventDefault()));
-const btn=document.querySelector('.menu'),nav=document.querySelector('nav');
-btn?.addEventListener('click',()=>nav.classList.toggle('open'));
+document.querySelector('#menu').onclick=()=>document.querySelector('nav').classList.toggle('open');document.querySelectorAll('a[href="#"]').forEach(a=>a.onclick=e=>e.preventDefault());
